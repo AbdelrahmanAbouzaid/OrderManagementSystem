85ac44c1c8c59d3da83ce502bf5f98ae62e1f712
Abdelrahman Abouzaid <a.abouzaid404@gmail.com>
2025-07-27 09:35:06 +0300

    Add Product Services

---
+++ Core/Services.Abstractions/IProductService.cs
@@ -0,0 +1,13 @@
+﻿
+using Shared.DTOs;
+
+namespace Services.Abstractions
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<ProductDto> GetProductByIdAsync(int productId);
+        Task<ProductDto> CreateProductAsync(CreateProductDto createProductDto);
+        Task<ProductDto> UpdateProductAsync(int productId, UpdateProductDto updateProductDto);
+    }
+}
---
+++ Shared/DTOs/UpdateProductDto.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Shared.DTOs
+{
+    public class UpdateProductDto
+    {
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
